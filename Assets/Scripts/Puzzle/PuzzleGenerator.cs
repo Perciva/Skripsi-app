61864931dbd669e523b5a81bae58d44f26140a6a
Karen <stromchild@gmail.com>
2023-03-20 08:40:43 +0700

    Puzzle 1

---
+++ Assets/Scripts/Puzzle/PuzzleGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzle
+{
+    public class PuzzleController
+    {
+        private List<Puzzle> _puzzles;
+        private Filebase fb;
+
+        public PuzzleController()
+        {
+            fb = Filebase.Instance;
+        }
+        
+        public void LoadPuzzles()
+        {
+            List<EncryptionPuzzle> enc= fb.LoadEncryptionPuzzles();
+            for (int i = 0; i < 4; i++)
+            {
+                int a = Random.Range(0, enc.Count);
+                _puzzles.Add(enc[a]);
+                enc.RemoveAt(a);
+            }
+            
+        }
+    }
+}
